f8264a1ba20e8d7688cf344cd7c0fa68cc45f21d
jklurfeld <jklurfeld@smith.edu>
2022-09-25 22:25:42 -0400

    finished coding

--- Conversation.java
+++ Conversation.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * This class takes in user input and responds based on detected mirror words.
+ */
 class Conversation {
-// String userInput = input.next();
-// String[] words = userInput.split(" ");
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
@@ -12,9 +13,11 @@ class Conversation {
     System.out.println("How many rounds?");
     int numRounds = input.nextInt();
 
+    //is it okay if I us ean arraylist rather than an array
     ArrayList<String> transcript = new ArrayList<String>();
 
     //it doens't like it when I put static in front of String[] cannedResponses
+    // why does it have to be static?
     String[] cannedResponses = {"Mhm.", "Ok.", "That's nice."};
 
     String greeting = "Hi there! What's on your mind?";
@@ -25,40 +28,43 @@ class Conversation {
     
     for (int i = 0; i < numRounds; i++){
       String dialogue = input.nextLine();
-      String response = dialogue;
-      int charactersLeft = dialogue.length();
 
-      if (dialogue.indexOf("I") != -1){
-        int locationOfWord = dialogue.indexOf("I");
-        response = response.substring(0, locationOfWord) + "you" + dialogue.substring(locationOfWord + 4);
-        charactersLeft -= (locationOfWord + 1);
-      }
-      if (dialogue.indexOf("me") != -1){
-        int locationOfWord = dialogue.indexOf("me");
-        response = response.substring(0, locationOfWord) + "you" + dialogue.substring(locationOfWord + 3);
-        charactersLeft -= (locationOfWord + 2);
-      }
-      if (dialogue.indexOf("am") != -1){
-        int locationOfWord = dialogue.indexOf("am");
-        response = response.substring(0, locationOfWord) + "are" + dialogue.substring(locationOfWord + 4);
-        charactersLeft -= (locationOfWord + 2);
-      }
-      if (dialogue.indexOf("you") != -1){
-        int locationOfWord = dialogue.indexOf("you");
-        response = response.substring(0, locationOfWord) + "I" + dialogue.substring(locationOfWord + 1);
-        charactersLeft -= (locationOfWord + 3);
-      }
-      if (dialogue.indexOf("my") != -1){
-        int locationOfWord = dialogue.indexOf("my");
-        response = response.substring(0, locationOfWord) + "your" + dialogue.substring(locationOfWord + 4);
-        charactersLeft -= (locationOfWord + 2);
-      }
-      if (dialogue.indexOf("your") != -1){
-        int locationOfWord = dialogue.indexOf("your");
-        response = response.substring(0, locationOfWord) + "my" + dialogue.substring(locationOfWord + 2);
-        charactersLeft -= (locationOfWord + 4);
+      String[] words = dialogue.split(" ");
+      for (int k = 0; k < words.length; k++){
+        if (words[k].equals("I")){
+          words[k] = "you";
+        }
+        else if (words[k].equals("me")){
+          words[k] = "you";
+        }
+        else if (words[k].equals("am")){
+          words[k] = "are";
+        }
+        else if (words[k].equals("you")){
+          words[k] = "I";
+        }
+        else if (words[k].equals("my")){
+          words[k] = "your";
+        }
+        else if (words[k].equals("your")){
+          words[k] = "my";
+        }
+        else if (words[k].equals("are")){
+          words[k] = "am";
+        }
       }
 
+      String response = "";
+      for (int j = 0; j < words.length; j++){
+        if (j != words.length - 1){
+          response += words[j] + " "; 
+        }
+        else {
+          response += words[j];
+        }
+      }
+
+      //if there are no mirror words, give a canned response
       if (dialogue.equals(response)){
         response = cannedResponses[(int)(Math.random() * 3)];
       }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,17 @@
 Use this file to record your reflection on this assignment.
+Jessica Klurfeld and Abby McIlraith
 
 - Which classmates did you consult with while working on this assignment?
+We consulted with Michelle Lawson
+
 - Which session(s) of TA / office hours did you attend?
+We attended the Monday (9/26) TA hours
+
 - What are your initial impressions of Java? 
+
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+A conclusion about programming is general is it's very useful to consult the java doc while working through programs. Using the tools in the java development kit helped make it clear why using classes was useful in programming instead of putting everything in one file.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+The split string method helped immensely in figuring out how to change the mirror words. Advice for someone taking this course in the future would be to be aware of case sensitivity when giving user input, to use print statements to debug your code, and to use your existing variables.
